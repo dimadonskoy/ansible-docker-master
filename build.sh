eb7788651cc1b8306cc0f0af424e9147dcd218a1
Dmitri Donskoy <crooper22@gmail.com>
2025-05-10 21:56:03 +0300

    build script added

--- build.sh
+++ build.sh
@@ -1,20 +1,29 @@
 #!/usr/bin/env bash
 
 #######################################################################
+# Developed by : Dmitri Donskoy
+# Purpose      : Ansible lightweight Docker master node
+# Update date  : 10.05.2025
+# Version      : 0.0.1
+#######################################################################
 
-#Developed by : Dmitri Donskoy
-#Purpose : Ansible lightweight docker master node 
-#Update date : 10.05.2025
-#Version : 0.0.1
-# set -x
 set -o errexit
 set -o nounset
 set -o pipefail
 
-############################ GLOBAL VARS ##############################
+# Check if docker-compose is installed
+command -v docker-compose >/dev/null 2>&1 || {
+  echo >&2 "docker-compose is not installed. Aborting."
+  exit 1
+}
 
-### Build image from Dockerfile
+# Logging function
+log() {
+  echo -e "\033[1;32m[INFO]\033[0m $1"
+}
+
+log "Building Docker image..."
 docker-compose build
 
-### Start the container
+log "Starting Docker containers..."
 docker-compose up -d
